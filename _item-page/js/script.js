214ade28a3c7a4c645186c2e3cc234d8e1169324
web-minder <67627648+Dmitriylies@users.noreply.github.com>
2020-08-28 14:36:33 +0300

    Add files via upload

---
+++ _item-page/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.item-page__item-title-1').on('click', function(){
+     $('.item-page__item-desc-wrap-1').toggleClass('active'); 
+     $('.item-page__item-title-1').toggleClass('active'); 
+      });
+   $('.item-page__item-title-2').on('click', function(){
+      $('.item-page__item-desc-wrap-2').toggleClass('active'); 
+      $('.item-page__item-title-2').toggleClass('active'); 
+      });
+});
